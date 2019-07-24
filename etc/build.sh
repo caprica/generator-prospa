0e8531700b521018ca7e28c2aa5da59804a07e00
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-07-24 20:32:32 +0100

    Add script files to generate and build all combinations of projects

---
+++ etc/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+languages=( "java" "kotlin" "scala" "groovy" )
+frameworks=( "javalin" "spring-boot" "spring-mvc" "vertx" )
+frontends=( "react" "angular" )
+
+pushd projects > /dev/null
+
+for language in "${languages[@]}" ; do
+
+    for framework in "${frameworks[@]}"; do
+
+        for frontend in "${frontends[@]}"; do
+
+            project="$language-$framework-$frontend"
+            echo "Building project '$project'..."
+
+            pushd $project > /dev/null
+
+            mvn clean install
+
+            popd > /dev/null
+            echo
+
+        done
+
+    done
+
+done
+
+popd > /dev/null
---
+++ etc/gen.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+javaVersion=11
+serverPort=8080
+
+languages=( "java" "kotlin" "scala" "groovy" )
+frameworks=( "javalin" "spring-boot" "spring-mvc" "vertx" )
+frontends=( "react" "angular" )
+
+rm -rf projects
+rm -rf specs
+
+mkdir projects
+mkdir specs
+
+pushd projects > /dev/null
+
+for language in "${languages[@]}" ; do
+
+    for framework in "${frameworks[@]}"; do
+
+        for frontend in "${frontends[@]}"; do
+
+            project="$language-$framework-$frontend"
+            echo "Creating project '$project'..."
+
+            spec="../../specs/$project.json"
+
+            mkdir $project
+            pushd $project > /dev/null
+
+            echo "{" >> $spec
+            echo "  \"groupId\"       : \"uk.co.caprica\"," >> $spec
+            echo "  \"name\"          : \"spa-test\"," >> $spec
+            echo "  \"projectVersion\": \"1.0.0-SNAPSHOT\"," >> $spec
+            echo "  \"description\"   : \"SPA $language $framework $frontend\"," >> $spec
+            echo "  \"language\"      : \"$language\"," >> $spec
+            echo "  \"javaVersion\"   : \"$javaVersion\"," >> $spec
+            echo "  \"framework\"     : \"$framework\"," >> $spec
+            echo "  \"frontEnd\"      : \"$frontend\"," >> $spec
+            echo "  \"packageName\"   : \"uk.co.caprica.spatest\"," >> $spec
+            echo "  \"mainClassName\" : \"Application\"," >> $spec
+            echo "  \"serverPort\"    : $serverPort" >> $spec
+            echo "}" >> $spec
+
+            yo prospa --spec "$spec" --force
+
+            popd > /dev/null
+            echo
+
+        done
+
+    done
+
+done
+
+popd > /dev/null
