5a862842c89746ca95019f1a5863519e2e31e974
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-07-27 10:19:45 +0100

    Clean up index/home components

--- generators/app/templates/front-end/vue/src/views/home/Home.vue
+++ generators/app/templates/front-end/vue/src/views/home/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Home</h1>
     <p>
-        VueJS Client Routing and SpringMVC Server Routing.
+        VueJS Single Page Application.
     </p>
     <span><router-link class="App-link" to="/users">Users</router-link> | <router-link class="App-link" to="/no/such/route">No Such Route</router-link></span>
   </div>
